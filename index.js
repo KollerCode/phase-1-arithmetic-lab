4348d99f7f4fcb42a40561d3963127452dbd9829
Natasha Koller <kollernatasha@gmail.com>
2021-11-26 19:15:27 -0500

    commit tree eb1e4fd0aaac34be0a59a709b03ea633d8ced8a3

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10) + 1;
+
+let num3 = 44;
+let num4 = 10;
+const mod = num3 % num4;
+
+const max = Math.max(10, 15, 20);
